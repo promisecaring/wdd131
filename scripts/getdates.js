86dd852aa8dbfc18444307803290660bf3611889
promisecaring <poghene@byupathway.edu>
2026-05-09 20:57:30 +0100

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+
+// getdates.js
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
